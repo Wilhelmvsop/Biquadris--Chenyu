20dc1f45b4a89584789c92544f506526e6959ddc
c63wei <c63wei@uwaterloo.ca>
2025-11-24 13:52:18 -0500

    specify input test name

--- tests/test_input.cc
+++ tests/test_input.cc
@@ -6,54 +6,72 @@ using namespace std;
 
 InputHandler ih;
 
-TEST_CASE(Commands) {
+std::vector<std::pair<std::string, std::string>> aliases = {
+	{"left", "left"},
+	{"right", "right"},
+	{"down", "down"},
+	{"drop", "drop"},
+	{"clockwise", "clockwise"},
+	{"cw", "clockwise"},
+	{"clock", "clockwise"},
+	{"counterclockwise", "counterclockwise"},
+	{"ccw", "counterclockwise"},
+	{"levelup", "levelup"},
+	{"leveldown", "leveldown"},
+	{"norandom", "norandom"},
+	{"random", "random"},
+	{"sequence", "sequence"},
+	{"restart", "restart"},
+	// Block-replacement single-letter commands
+	{"I", "I"},
+	{"J", "J"},
+	{"L", "L"},
+	{"O", "O"},
+	{"S", "S"},
+	{"Z", "Z"},
+	{"T", "T"}};
+
+TEST_CASE(InputHandler_register)
+{
 	// Register canonical commands and common aliases from the spec
-	std::vector<std::pair<std::string, std::string>> aliases = {
-		{"left", "left"},
-		{"right", "right"},
-		{"down", "down"},
-		{"drop", "drop"},
-		{"clockwise", "clockwise"}, {"cw", "clockwise"}, {"clock", "clockwise"},
-		{"counterclockwise", "counterclockwise"}, {"ccw", "counterclockwise"},
-		{"levelup", "levelup"}, {"leveldown", "leveldown"},
-		{"norandom", "norandom"}, {"random", "random"},
-		{"sequence", "sequence"}, {"restart", "restart"},
-		// Block-replacement single-letter commands
-		{"I", "I"}, {"J", "J"}, {"L", "L"}, {"O", "O"},
-		{"S", "S"}, {"Z", "Z"}, {"T", "T"}
-	};
 
 	// Register aliases
-	for (auto &p : aliases) {
+	for (auto &p : aliases)
+	{
 		REQUIRE(ih.registerAlias(p.first, p.second));
 	}
+}
 
-	// resolve checks: exact matches
-	for (auto &p : aliases) {
+TEST_CASE(InputHandler_resolve)
+{ // resolve checks: exact matches
+	for (auto &p : aliases)
+	{
 		REQUIRE_EQUAL(p.second, ih.resolve(p.first));
+		// Unique-prefix resolution checks
+		// "levelup" vs "leveldown" -> "lev" ambiguous, specific prefixes should resolve
+		REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("lev"));
+		REQUIRE_EQUAL(std::string("levelup"), ih.resolve("levelu"));
+		REQUIRE_EQUAL(std::string("leveldown"), ih.resolve("leveld"));
+
+		// Single-letter prefix 'r' is ambiguous because of "right" and "random"
+		REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("r"));
+		REQUIRE_EQUAL(std::string("norandom"), ih.resolve("n"));
+
+		// Rename an alias: move "restart" to "reboot" and verify
+		REQUIRE(ih.registerAlias("temp_restart", "restart"));
+		REQUIRE_EQUAL(std::string("restart"), ih.resolve("temp_restart"));
+
+		// multiplier: prefix digits should be accepted for commands that allow multipliers
+		REQUIRE_EQUAL(std::string("left"), ih.resolve("3lef"));
+		REQUIRE_EQUAL(std::string("right"), ih.resolve("3ri"));
+		// multiplier disallowed for restart -> invalid
+		REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("3restart"));
 	}
+}
 
-	// Unique-prefix resolution checks
-	// "levelup" vs "leveldown" -> "lev" ambiguous, specific prefixes should resolve
-	REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("lev"));
-	REQUIRE_EQUAL(std::string("levelup"), ih.resolve("levelu"));
-	REQUIRE_EQUAL(std::string("leveldown"), ih.resolve("leveld"));
-
-	// Single-letter prefix 'r' is ambiguous because of "right" and "random"
-	REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("r"));
-	REQUIRE_EQUAL(std::string("norandom"), ih.resolve("n"));
-
-	// Rename an alias: move "restart" to "reboot" and verify
-	REQUIRE(ih.registerAlias("temp_restart", "restart"));
-	REQUIRE_EQUAL(std::string("restart"), ih.resolve("temp_restart"));
+TEST_CASE(InputHandler_rename)
+{
 	REQUIRE(ih.rename("temp_restart", "reboot"));
 	REQUIRE_EQUAL(std::string("restart"), ih.resolve("reboot"));
 	REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("temp_restart"));
-
-	// multiplier: prefix digits should be accepted for commands that allow multipliers
-	REQUIRE_EQUAL(std::string("left"), ih.resolve("3lef"));
-	REQUIRE_EQUAL(std::string("right"), ih.resolve("3ri"));
-	// multiplier disallowed for restart -> invalid
-	REQUIRE_EQUAL(std::string("INVALID_CMD"), ih.resolve("3restart"));
 }
-
