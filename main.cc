ef4021bf17ebaee79e1dcd4570e022ec8fd3e9ab
WaronLimsakul <poonro@gmail.com>
2025-11-27 17:55:11 -0500

    refactor: update main::parseFlag

--- main.cc
+++ main.cc
@@ -6,54 +6,62 @@ import <string>;
 import <vector>;
 import <memory>;
 
+bool parseFlag(GameSettings& settings, const std::vector<std::string>& args);
+
 int main(int argc, char* argv[]) {
-    bool tuiOnly = false;
-    unsigned int seed = 67;
-    std::string scriptfile1 = "sequence1.txt";
-    std::string scriptfile2 = "sequence2.txt";
-    int startLevel = 0;
+    GameSettings settings{
+        false,           67, "sequence1.txt",
+        "sequence2.txt", 0,  std::make_shared<InputHandler>()};
 
     std::vector<std::string> args{};
     for (int i = 1; i < argc; ++i) {
         args.emplace_back(argv[i]);
     }
 
-    for (int i = 0; i < args.size(); ++i) {
-        const std::string& flag = args[i];
-        if (flag == "-text") {
-            tuiOnly = true;
-        } else if (flag == "-seed") {
-            if (i + 1 >= args.size()) {
-                std::cerr << "Go goon" << std::endl;
-                return 1;
-            }
-            seed = std::stoul(args[++i]);
-        } else if (flag == "-scriptfile1") {
-            if (i + 1 >= args.size()) {
-                std::cerr << "Go goon" << std::endl;
-                return 1;
-            }
-            scriptfile1 = args[++i];
-        } else if (flag == "-scriptfile2") {
-            if (i + 1 >= args.size()) {
-                std::cerr << "Go goon" << std::endl;
-                return 1;
-            }
-            scriptfile2 = args[++i];
-        } else if (flag == "-startlevel") {
-            if (i + 1 >= args.size()) {
-                std::cerr << "Go goon" << std::endl;
-                return 1;
-            }
-            startLevel = std::stoi(args[++i]);
-        } else {
-            std::cerr << "Go GOON" << std::endl;
-            return 1;
-        }
+    if (!parseFlag(settings, args)) {
+        return 1;
     }
-
-    Game game{{tuiOnly, seed, scriptfile1, scriptfile2, startLevel,
-               std::make_shared<InputHandler>()}};
+    Game game{settings};
     game.play();
     return 0;
 }
+
+bool parseFlag(GameSettings& settings, const std::vector<std::string>& args) {
+    for (unsigned int i = 0; i < args.size(); ++i) {
+        const std::string& flag = args[i];
+        if (flag == "-text") {
+            settings.tuiOnly = true;
+        } else if (flag == "-seed") {
+            if (i + 1 >= args.size()) {
+                std::cerr << "seed: please provide seed" << std::endl;
+                return false;
+            }
+            settings.seed = std::stoul(args[++i]);
+        } else if (flag == "-scriptfile1") {
+            if (i + 1 >= args.size()) {
+                std::cerr << "scriptfile: please provide scriptfile"
+                          << std::endl;
+                return false;
+            }
+            settings.script1 = args[++i];
+        } else if (flag == "-scriptfile2") {
+            if (i + 1 >= args.size()) {
+                std::cerr << "scriptfile: please provide scriptfile"
+                          << std::endl;
+                return false;
+            }
+            settings.script2 = args[++i];
+        } else if (flag == "-startlevel") {
+            if (i + 1 >= args.size()) {
+                std::cerr << "startlevel: please provide the start level"
+                          << std::endl;
+                return false;
+            }
+            settings.startLevel = std::stoi(args[++i]);
+        } else {
+            std::cerr << "invalid flag " << flag << std::endl;
+            return false;
+        }
+    }
+    return true;
+};
